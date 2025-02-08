f6966209388f38e657c900682ec934ef410caa21
Vincent Guyader <vincent@thinkr.fr>
2025-02-08 22:50:11 +0000

    version 0.4.4

--- inst/doc/b-bookdown-and-scripts.R
+++ inst/doc/b-bookdown-and-scripts.R
@@ -8,41 +8,41 @@ knitr::opts_chunk$set(
 library(attachment)
 
 ## ----eval=FALSE---------------------------------------------------------------
-# usethis::use_description()
+#  usethis::use_description()
 
 ## ----eval=FALSE---------------------------------------------------------------
-# # bookdown Imports are in Rmds
-# imports <- c("bookdown", attachment::att_from_rmds("."))
-# 
-# attachment::att_to_desc_from_is(
-#   path.d = "DESCRIPTION",
-#   imports = imports, suggests = NULL,
-#   must.exist = FALSE
-# )
+#  # bookdown Imports are in Rmds
+#  imports <- c("bookdown", attachment::att_from_rmds("."))
+#  
+#  attachment::att_to_desc_from_is(
+#    path.d = "DESCRIPTION",
+#    imports = imports, suggests = NULL,
+#    must.exist = FALSE
+#  )
 
 ## ----eval=FALSE---------------------------------------------------------------
-# # install.packages("remotes")
-# remotes::install_deps()
+#  # install.packages("remotes")
+#  remotes::install_deps()
 
 ## ----eval=FALSE---------------------------------------------------------------
-# attachment::att_to_desc_from_is(
-#   path.d = "DESCRIPTION",
-#   imports = imports, suggests = NULL,
-#   must.exist = FALSE
-# ) %>%
-#   set_remotes_to_desc()
+#  attachment::att_to_desc_from_is(
+#    path.d = "DESCRIPTION",
+#    imports = imports, suggests = NULL,
+#    must.exist = FALSE
+#  ) %>%
+#    set_remotes_to_desc()
 
 ## ----eval=FALSE---------------------------------------------------------------
-# att_from_namespace()
+#  att_from_namespace()
 
 ## ----eval=FALSE---------------------------------------------------------------
-# att_from_rscripts()
+#  att_from_rscripts()
 
 ## ----eval=FALSE---------------------------------------------------------------
-# att_from_examples()
+#  att_from_examples()
 
 ## ----eval=FALSE---------------------------------------------------------------
-# att_from_rmds()
+#  att_from_rmds()
 
 ## ----eval=TRUE----------------------------------------------------------------
 dummypackage <- system.file("dummypackage", package = "attachment")
--- inst/doc/use_renv.R
+++ inst/doc/use_renv.R
@@ -11,10 +11,10 @@ library(attachment)
 attachment:::extra_dev_pkg
 
 ## ----eval=FALSE---------------------------------------------------------------
-# create_renv_for_dev() # with all default above
-# create_renv_for_dev(dev_pkg = "attachment") # with {attachment} only
-# create_renv_for_dev(dev_pkg = c("_default", "DT")) # for all default and {DT}
+#  create_renv_for_dev() # with all default above
+#  create_renv_for_dev(dev_pkg = "attachment") # with {attachment} only
+#  create_renv_for_dev(dev_pkg = c("_default", "DT")) # for all default and {DT}
 
 ## ----eval=FALSE---------------------------------------------------------------
-# create_renv_for_prod(output = "renv.lock.prod")
+#  create_renv_for_prod(output = "renv.lock.prod")
 
